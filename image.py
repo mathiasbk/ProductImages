016ae22794c0125f47611ce21f1e3d26f81cd76f
Mathias <mathiasklow@gmail.com>
2025-01-14 15:44:50 +0100

    Fixed problems with imagemode when saved as jpg

--- image.py
+++ image.py
@@ -4,6 +4,7 @@ from PIL import Image
 def DownloadImages(images):
 
     downloaded_images = []
+
     # Create a directory for the images
     if not os.path.exists("images"):
         os.makedirs("images")
@@ -23,7 +24,7 @@ def DownloadImages(images):
             # Get filetype from the response headers
             extension = response.headers.get("Content-Type", "").split("/")[-1]
 
-            # Generer filnavn
+            # Generate filename
             filename = os.path.basename(urlsplit(img).path)
             if not filename:
                 filename = "downloaded_image"
@@ -31,7 +32,7 @@ def DownloadImages(images):
                 filename += "."+extension
             print("Filename: ", filename)
 
-            # Full sti for filen
+            # Full path to the file
             file_path = os.path.join("images", filename)
 
             #save the image
@@ -53,16 +54,26 @@ def ChangeFiletype(images, format):
         if format == os.path.splitext(os.path.basename(img))[1][1:]:
             continue
 
+        #Save the old filename
         old_file = img
+
         f, e = os.path.splitext(os.path.basename(img))
         output_path = os.path.join("images", f + "." + format)
-        #url_path = urlsplit(img).path
         
+        #Check filetype
+        pil_format = format.upper()
+        if pil_format == "JPG":
+            pil_format = "JPEG"
+
         #Open the image
         image = Image.open(img)
         
+        #Convert to RGB if we want to save as jpeg
+        if pil_format == "JPEG" and image.mode == "RGBA":
+            image = image.convert("RGB")
+
         #Save the new file with correct format
-        image.save(output_path, format = format.upper())
+        image.save(output_path, format = pil_format)
 
         #Delete the old image with wrong format
         os.remove(old_file)
